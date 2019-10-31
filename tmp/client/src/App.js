4edbe7fe131da2ea831ac5a65d48bff3941fac34
r4pt0s <r4pt0s@protonmail.com>
2019-10-31 09:42:49 +0100

    workaround

---
+++ tmp/client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1 className="App-title">Welcome to React</h1>
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
